a5c3fac3688e372f9e781ce5c4ec99928d4c7a8b
Zubair Sheikh <izubairsh@gmail.com>
2022-01-17 02:24:01 +0500

    changes

--- Sources/Utils/Extensions/StringExtensions.swift
+++ Sources/Utils/Extensions/StringExtensions.swift
@@ -10,23 +10,23 @@ import Foundation
 
 extension String {
 
-    func toDate(withFormat format: String = "yyyy-MM-dd")-> Date?{
+    public func toDate(withFormat format: String = "yyyy-MM-dd")-> Date?{
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = format
         let date = dateFormatter.date(from: self)
         return date
     }
 
-    func isValid(field: FieldType) -> Bool {
+    public func isValid(field: FieldType) -> Bool {
         let pred = NSPredicate(format:"SELF MATCHES %@", field.getRegix())
         return pred.evaluate(with: self)
     }
     
-    func toURL() -> URL? {
+    public func toURL() -> URL? {
         return URL(string: self)
     }
 
-    func localized() -> String {
+    public func localized() -> String {
         let path = Bundle.main.path(forResource: UserDefaults.standard.string(forKey: "language"), ofType: "lproj")
         let bundle = Bundle(path: path!)
         return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
