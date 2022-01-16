1e9756d6824b170bf0cf49cd3fabb55905eca251
Zubair Sheikh <izubairsh@gmail.com>
2022-01-17 03:14:54 +0500

    changes

--- Sources/Utils/Constants/AppConstants.swift
+++ Sources/Utils/Constants/AppConstants.swift
@@ -7,113 +7,112 @@
 
 import Foundation
 
-class AppKeys {
-    static let username = "username"
-    static let isAuthenticated = "isAuthenticated"
-    static let accessToken = "accessToken"
-    static let refreshToken = "refreshToken"
-    static let userId = "userId"
-    static let openProfile = "openProfile"
-    static let isTap = "isTap"
-    static let refresh = "refresh"
-    static let language = "language"
+public class AppKeys {
+    public static let username = "username"
+    public static let isAuthenticated = "isAuthenticated"
+    public static let accessToken = "accessToken"
+    public static let refreshToken = "refreshToken"
+    public static let userId = "userId"
+    public static let openProfile = "openProfile"
+    public static let isTap = "isTap"
+    public static let refresh = "refresh"
+    public static let language = "language"
 }
 
-class AppStrings {
-    static let business = "Business"
-    static let personal = "Personal"
-    static let changeLanguage = "Change language"
-    static let selectLanguage = "Select language"
+public class AppStrings {
+    public static let business = "Business"
+    public static let personal = "Personal"
+    public static let changeLanguage = "Change language"
+    public static let selectLanguage = "Select language"
     
-    static let setUpYourTap = "Set Up Your appname"
-    static let buyATap = "Buy a appname"
-    static let welcomeToTap = "Welcome to appname"
-    static let alreadyHaveTap = "Already have appname?"
+    public static let setUpYourTap = "Set Up Your appname"
+    public static let buyATap = "Buy a appname"
+    public static let welcomeToTap = "Welcome to appname"
+    public static let alreadyHaveTap = "Already have appname?"
     
+    public static let instantlyShareAnything = "New beginnings every tap"
+    public static let signIn = "Sign in"
+    public static let forgotPassword = "Forgot Password?"
     
-    static let instantlyShareAnything = "New beginnings every tap"
-    static let signIn = "Sign in"
-    static let forgotPassword = "Forgot Password?"
+    public static let welcomeBack = "Welcome Back"
     
-    static let welcomeBack = "Welcome Back"
+    public static let email = "Email"
+    public static let name = "Name & Surname"
+    public static let bio = "Write your bio"
+    public static let location = "Location"
+    public static let gender = "Gender"
+    public static let dateOfBirth = "Date of Birth"
+    public static let username = "Username"
+    public static let password = "Password"
+    public static let confirmPassword = "Confirm Password"
+    public static let changePassword = "Change Password"
+    public static let back = "back"
+    public static let backToLogin = "Back To Login"
+    public static let generateLink = "Generate Link"
+    public static let enterEmailToResetPassword = "Enter your appname email and we'll send you a reset link"
+    public static let createYourProfile = "Create Your Profile"
     
-    static let email = "Email"
-    static let name = "Name & Surname"
-    static let bio = "Write your bio"
-    static let location = "Location"
-    static let gender = "Gender"
-    static let dateOfBirth = "Date of Birth"
-    static let username = "Username"
-    static let password = "Password"
-    static let confirmPassword = "Confirm Password"
-    static let changePassword = "Change Password"
-    static let back = "back"
-    static let backToLogin = "Back To Login"
-    static let generateLink = "Generate Link"
-    static let enterEmailToResetPassword = "Enter your appname email and we'll send you a reset link"
-    static let createYourProfile = "Create Your Profile"
+    public static let almostDoneCreatingAccount = "Almost Done! Create a username for your account"
     
-    static let almostDoneCreatingAccount = "Almost Done! Create a username for your account"
+    public static let signUp = "Sign Up"
+    public static let saveProfile = "Save Profile"
+    public static let continueText = "Continue"
+    public static let byRegistering = "By registering, you agree to our"
+    public static let termsAndConditions = "Terms and Privacy policy"
+    public static let path = "Path"
+    public static let save = "Save"
+    public static let update = "Update"
+    public static let delete = "Delete"
+    public static let open = "Open"
+    public static let search = "Search..."
+    public static let done = "Done"
     
-    static let signUp = "Sign Up"
-    static let saveProfile = "Save Profile"
-    static let continueText = "Continue"
-    static let byRegistering = "By registering, you agree to our"
-    static let termsAndConditions = "Terms and Privacy policy"
-    static let path = "Path"
-    static let save = "Save"
-    static let update = "Update"
-    static let delete = "Delete"
-    static let open = "Open"
-    static let search = "Search..."
-    static let done = "Done"
+    public static let directOn = "Direct On"
+    public static let directOff = "Direct Off"
+    public static let editProfile = "Edit Profile"
+    public static let editBusiness = "Edit Business"
+    public static let yourTapOpensDirectly = "Your appname opens Directly to Your first link"
     
-    static let directOn = "Direct On"
-    static let directOff = "Direct Off"
-    static let editProfile = "Edit Profile"
-    static let editBusiness = "Edit Business"
-    static let yourTapOpensDirectly = "Your appname opens Directly to Your first link"
-    
-    static let makeProfilePrivate = "Make Profile Private"
-    
-    
-    static let othersCanViewYourProfile = "Others can view your profile using the link"
-    
-    static let home = "Home"
-    static let activate = "Activate"
-    static let readTap = "Read appname"
-    static let qrCode = "My QR code"
-    static let howToTap = "How to appname"
-    static let tapAmbassador = "appname Ambassador"
-    static let buyTap = "Buy appname"
-    static let settings = "Settings"
-    static let logout = "Log out"
-    
-    static let whichTapProductWouldYou = "Choose the device you are activating"
+    public static let makeProfilePrivate = "Make Profile Private"
     
     
-    static let image = "Image"
-    static let pickImageSource = "Pick image source"
-    static let camera = "Camera"
-    static let photoLibrary = "Photo Library"
+    public static let othersCanViewYourProfile = "Others can view your profile using the link"
     
-    static let disconnect = "Disconnect"
-    static let connect = "Connect"
-    static let privateProfile = "This profile is private please connect to view accounts."
-    static let profileNotFound = "Profile Not found..."
+    public static let home = "Home"
+    public static let activate = "Activate"
+    public static let readTap = "Read appname"
+    public static let qrCode = "My QR code"
+    public static let howToTap = "How to appname"
+    public static let tapAmbassador = "appname Ambassador"
+    public static let buyTap = "Buy appname"
+    public static let settings = "Settings"
+    public static let logout = "Log out"
     
-    static let shareProfileLink = "Share profile link"
-    static let shareQRCode = "Share QR Code"
-    static let holdToScan = "Hold your iphone near to scan."
-    static let holdToActivate = "Hold your iphone near to activate."
+    public static let whichTapProductWouldYou = "Choose the device you are activating"
+    
+    
+    public static let image = "Image"
+    public static let pickImageSource = "Pick image source"
+    public static let camera = "Camera"
+    public static let photoLibrary = "Photo Library"
+    
+    public static let disconnect = "Disconnect"
+    public static let connect = "Connect"
+    public static let privateProfile = "This profile is private please connect to view accounts."
+    public static let profileNotFound = "Profile Not found..."
+    
+    public static let shareProfileLink = "Share profile link"
+    public static let shareQRCode = "Share QR Code"
+    public static let holdToScan = "Hold your iphone near to scan."
+    public static let holdToActivate = "Hold your iphone near to activate."
 }
 
 
-class AlertStrings {
-    static let inValidEmail = "Please enter valid email"
-    static let inValidPassword = "Password has least one uppercase, one digit, one lowercase, one symbol and should be min 8 characters total"
+public class AlertStrings {
+    public static let inValidEmail = "Please enter valid email"
+    public static let inValidPassword = "Password has least one uppercase, one digit, one lowercase, one symbol and should be min 8 characters total"
     
-    static let networkError = "Error is You are not connected to the internet please check your settings."
+    public static let networkError = "Error is You are not connected to the internet please check your settings."
 }
 
 
