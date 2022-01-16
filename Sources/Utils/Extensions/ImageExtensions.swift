188864fa8eb2ec453f518a5f843e4f68571be7db
Zubair Sheikh <izubairsh@gmail.com>
2022-01-17 01:52:02 +0500

    changes

--- Sources/Utils/Extensions/ImageExtensions.swift
+++ Sources/Utils/Extensions/ImageExtensions.swift
@@ -1,37 +1,37 @@
 //
 //  ImageExtensions.swift
-//  osbc
+//  Domiklik
 //
 //  Created by Zubair Sheikh on 15/06/2021.
 //
 
-import Foundation
-import SwiftUI
 import SDWebImageSwiftUI
+import SwiftUI
 
 extension Image {
-    open func circleImageModifier(width: CGFloat = 200, height: CGFloat = 200, padding: Bool = false, backgroundColor: Color = Color.black) -> some View {
+    func circleImageModifier(width: CGFloat = 200, height: CGFloat = 200, padding: CGFloat = 0, backgroundColor: Color = Color.white, foregroundColor: Color = .black) -> some View {
         self
             .resizable()
             .frame(width: width, height: height)
-            .aspectRatio(1.0, contentMode: .fit)
-            .padding(padding ? 10 : 0)
+            .scaledToFit()
+            .padding(padding)
+            .foregroundColor(foregroundColor)
             .background(backgroundColor)
             .clipShape(Circle())
-    }
+            .shadow(radius: 10)
+   }
 }
 
 extension WebImage {
-    open func circleImageModifier(width: CGFloat = 180, height: CGFloat = 180, padding: CGFloat = 0) -> some View {
+    func circleImageModifier(width: CGFloat = 200, height: CGFloat = 200, padding: CGFloat = 0) -> some View {
         self
             .resizable()
             .placeholder(Image(systemName: "person"))
             .frame(width: width, height: height)
-            .aspectRatio(1.0, contentMode: .fit)
+            .scaledToFit()
             .padding(padding)
             .background(Color.white)
             .clipShape(Circle())
             .shadow(radius: 10)
-            .overlay(Circle().stroke(Color.white, lineWidth: 1))
-    }
+   }
 }
