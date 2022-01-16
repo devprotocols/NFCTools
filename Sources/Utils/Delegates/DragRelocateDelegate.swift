0b4fcb94fceec05605c2c7ed901646fd88cb7eab
Zubair Sheikh <izubairsh@gmail.com>
2022-01-17 03:34:18 +0500

    changes

--- Sources/Utils/Delegates/DragRelocateDelegate.swift
+++ Sources/Utils/Delegates/DragRelocateDelegate.swift
@@ -12,6 +12,13 @@ public struct DragRelocateDelegate<T: BaseCodable>: DropDelegate {
     public let item: T
     @Binding public var listData: [T]
     @Binding public var current: T?
+    
+    public init(item: T, listData: Binding<[T]>, current: Binding<T?>){
+        self.item = item
+        self._listData = listData
+        self._current = current
+    }
+    
     public func dropEntered(info: DropInfo) {
         if item != current {
             guard let from = listData.firstIndex(of: current!) else {return}
